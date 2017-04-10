6036f882fdf93859db367f3c6f4c33cd257e55a0
Neelesh Roy <neeleshroy.2013@gmail.com>
2017-04-10 20:59:48 +0530

    Added click toggles Added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('\'Allo \'Allo!');
+let power = true;
+
+function changeMetric() {
+  $('.celsius').click(function() {
+    $('.farenheit').addClass('metric-deselected');
+    $('.celsius').removeClass('metric-deselected');
+    $('.temperature p').html('38<sup>&deg</sup>');
+  });
+  $('.farenheit').click(function() {
+    $('.celsius').addClass('metric-deselected');
+    $('.farenheit').removeClass('metric-deselected');
+    $('.temperature p').html('102<sup>&deg</sup>');
+  });
+}
+
+function switchPower(){
+  $('.power-button').click(() => {
+    if(power) {
+      $('.power-button').attr('src', 'images/power-off.svg');
+      power = false;
+    } else {
+      $('.power-button').attr('src', 'images/power-on.svg');
+      power = true;
+    }
+  });
+}
+
+switchPower();
+changeMetric();
